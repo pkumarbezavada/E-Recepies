59c7bf13f4b0364f5bc06a3fb36354126b06c110
PRASANNA KUMAR BEZAVADA <bprasanna.555@gmail.com>
2018-08-06 13:46:35 -0700

    updated index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Router from "./components/Router";
+import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<Router />, document.getElementById('root'));
+ReactDOM.render(<Router basename= {process.env.PUBLIC_URL} ><App /></Router>, document.getElementById('root'));
 registerServiceWorker();
